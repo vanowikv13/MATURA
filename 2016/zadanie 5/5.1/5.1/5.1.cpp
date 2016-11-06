e05cb9fef22f9f357b37e3d87a27aad56fe5b347
vanowikv13 <vanowikv13@gmil.com>
2016-11-06 11:36:59 +0100

    proprawienie błędów

--- 2016/zadanie 5/5.1/5.1/5.1.cpp
+++ 2016/zadanie 5/5.1/5.1/5.1.cpp
@@ -10,7 +10,12 @@ using namespace std;
 
 struct Student {
 	string imie, nazwisko, pesel;
+	int ilewys;
+	vector<string>listbook;
+};
 
+struct wypo {
+	string pesel, ksiazka;
 };
 
 fstream plik; //plik do pobierania danych
@@ -28,12 +33,11 @@ void wypisz(Student * obiekt) {
 
 int main()
 {
-	vector<Student> stud;
+	vector<Student> stud; //zawsze zaczynaj od jeden
 	plik.open("studenci.txt",ios::in);
 	string napis;
 
 	//pobieranie danych z pliku
-	getline(plik, napis);
 	while (getline(plik, napis)) {
 
 		 Student ad;
@@ -56,9 +60,62 @@ int main()
 		 }
 
 		 ad.imie = copyx(pocz, kon, napis);
+		 ad.ilewys = 0;
 		 stud.push_back(ad);
+		 
 
 	}
+	plik.close();
+	plik.open("wypozyczenia.txt", ios::in);
+
+	vector<wypo> wyp;//zawsze zaczynaj od jeden
+	while (getline(plik, napis)) {
+
+		wypo reno;
+		bool hel = false;
+		int pocz = 0, kon = napis.size(), i = 1;
+		for (int i = 0; i < kon; i++) {
+			if (napis[i] == '\t' && hel) {
+				reno.pesel = copyx(pocz, i, napis);
+				pocz = i + 1;
+			}
+			else if (napis[i] == '\t' && !hel) {
+				pocz = i+1;
+				hel = true;
+			}
+
+		}
+		reno.ksiazka = copyx(pocz, kon, napis);
+		pocz = 0; hel = false;
+
+		wyp.push_back(reno);
+		
+
+	}
+
+	//szukanie wystopien
+	int bestintsize = 0; vector<Student>::iterator adad;
+	for (vector<Student>::iterator it = stud.begin()+1; it < stud.end();it++) {
+		for (auto itw = wyp.begin() + 1; itw < wyp.end(); itw++) { //kontrukcja auto jest naprawde wygodna
+			if (it->pesel == itw->pesel) {
+				it->ilewys++;
+				it->listbook.push_back(itw->ksiazka);
+			}
+		}
+		if (bestintsize < it->ilewys) {
+			bestintsize = it->ilewys;
+			adad = it;
+		}
+	}
+
+	//wypisywanie
+	cout << adad->imie << " " << adad->nazwisko << "liczba ksiazek : " << endl;
+	for (int i = 0; i < adad->ilewys; i++)
+		cout << "- " << adad->listbook[i] << endl;
+
+
+
+	plik.close();
 	system("pause");
     return 0;
 }
